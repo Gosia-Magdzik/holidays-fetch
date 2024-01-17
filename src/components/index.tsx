9bdc3b24bd13ab0b181b2b8fdc35384711268466
Gosia Magdzik <melassa26@gmail.com>
2024-01-17 15:06:05 +0100

    fetchAPI created

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,8 +1,25 @@
 import React, { useState } from 'react'
+import { APIurl } from '../api_links'
 
 export const UsHolidays = () => {
 
   const [apiData, setApiData] = React.useState();
+
+  React.useEffect(() => {
+    fetchAPI()
+  }, []);
+
+  const fetchAPI = async () => {
+    try {
+      const response = await fetch(APIurl);
+      const data = await response.json();
+      setApiData(data);
+      console.log(data)
+    }
+    catch (error) {
+      console.error('XXX')
+    }
+  }
 
   return (
     <div>UsHolidays</div>
